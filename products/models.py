bbabdad2f9da5759f210f587051cab465f7ed143
Franker88 <franker5588@gmail.com>
2026-03-25 15:35:49 -0400

    CREATE product item

---
+++ products/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+class Product(models.Model):
+    name = models.TextField(max_length=255, verbose_name='Product name')
+    description = models.TextField(max_length=300, verbose_name='Product description')
+    price = models.DecimalField(max_digits=10, decimal_places=2, verbose_name='Product price')
+    available = models.BooleanField(default=True, verbose_name='Available')
+    image = models.ImageField(upload_to='products/', null=True, blank=True, verbose_name='Product image')
+
+    def __str__(self):
+        return self.name
